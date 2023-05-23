101887736c398e8ddadbb92b1181f8603a871ef2
IlhombekToshqulov <ilxomdeveloper@gmail.com>
2023-05-23 09:56:00 +0500

    Create: Temperatura

--- LeedCode/Temperatura.cs
+++ LeedCode/Temperatura.cs
@@ -1,8 +1,19 @@
-﻿using System;
-
-public class Class1
+﻿public class Program
 {
-	public Class1()
-	{
-	}
+    static void Main(string[] args)
+    {
+        double[] arr = ConvertTemperature(36.50);
+        foreach (double d in arr)
+            Console.WriteLine(d + " ");
+    }
+    public static double[] ConvertTemperature(double celsius)
+    {
+        double[] temperture = new double[2];
+        double kelvin = celsius + 273.15;
+        temperture[0] = kelvin;
+        double farangeyt = celsius * 1.8 + 32;
+        temperture[1] = farangeyt;
+        return temperture;
+    }
+
 }
