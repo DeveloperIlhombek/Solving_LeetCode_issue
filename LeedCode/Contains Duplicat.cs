a3989972c0813975274eb7d61780d6678a2025dc
IlhombekToshqulov <ilxomdeveloper@gmail.com>
2023-05-26 10:49:49 +0500

    Contains Duplicat

---
+++ LeedCode/Contains Duplicat.cs
@@ -0,0 +1,27 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arr = new int[] { 1, 2, 3, 1, 2, 3 };
+        int k = 2;
+        Console.WriteLine(ContainsNearbyDuplicate(arr, k));
+    }
+    public static bool ContainsNearbyDuplicate(int[] nums, int k)
+    {
+        int n = nums.Length - 1;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (nums[i] == nums[j] && Math.Abs(j - i) <= k)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
+
+
